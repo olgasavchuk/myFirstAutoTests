807094e450da1a301c705ae49a7f80e28346e358
olgasavchuk <olga.savchuk.pm@gmail.com>
2016-01-14 00:33:41 +0300

    class to check if it's run through the cmd

---
+++ src/test/java/com/chap003/TestCheckMvnCommandLine.java
@@ -0,0 +1,15 @@
+package com.chap003;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class TestCheckMvnCommandLine {
+
+    @Test
+    public void checkRunMethodFromCmd(){
+
+        assertTrue(2>1);
+    }
+}
