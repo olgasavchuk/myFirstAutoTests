12e9d8346569b166a6f25766619a62d99b6f99dc
olgasavchuk <olga.savchuk.pm@gmail.com>
2016-01-15 15:45:37 +0300

    Integer class testing

---
+++ src/test/java/com/chap004/IntegerExampleTest.java
@@ -0,0 +1,14 @@
+package com.chap004;
+
+import org.junit.Test;
+
+public class IntegerExampleTest {
+
+    @Test
+    public void integerExploration(){
+
+        Integer four = new Integer(4);
+
+    }
+
+}
