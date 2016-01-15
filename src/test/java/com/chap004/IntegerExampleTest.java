dda3cbb3ed60a53f5ff20c874434ef0a4c66e07a
olgasavchuk <olga.savchuk.pm@gmail.com>
2016-01-15 16:46:52 +0300

    assertEqual - intValue returns 4

--- src/test/java/com/chap004/IntegerExampleTest.java
+++ src/test/java/com/chap004/IntegerExampleTest.java
@@ -2,13 +2,15 @@ package com.chap004;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class IntegerExampleTest {
 
     @Test
     public void integerExploration(){
 
         Integer four = new Integer(4);
-
+        assertEquals("intValue returs 4", 4, four.intValue());
     }
 
 }
