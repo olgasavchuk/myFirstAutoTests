82b47bdd93b0b326dee216d44f16d318c16a1b10
olgasavchuk <olga.savchuk.pm@gmail.com>
2016-01-25 22:11:05 +0300

    incrementDecrementOperators just renamed the class

--- src/test/java/com/chap007/IncrementDecrementTest.java
+++ src/test/java/com/chap007/IncrementDecrementTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class IncretemntDecrementTest {
+public class IncrementDecrementTest {
 
     @Test
     public void incrementDecrementOperatorsExplored() {
