aef5aa204d3afcd5bcb840a577fbd0e2e81388e5
olgasavchuk <Qaolga1!>
2016-02-01 16:15:28 +0300

    MORE switch operator testing

--- src/test/java/com/chap008/SwitchTest.java
+++ src/test/java/com/chap008/SwitchTest.java
@@ -1,7 +1,6 @@
 package com.chap008;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class SwitchTest {
@@ -19,37 +18,89 @@ public class SwitchTest {
     }
 
     private String likelyGenderIs(String title) {
-        String likelyGender = new String();
-        title = title.toLowerCase();
 
-        switch(title){
+        String gender = new String();
+        switch(title.toLowerCase()){
             case "sir":
-                likelyGender = "M";
-                break;
             case "mr":
-                likelyGender = "M";
-                break;
             case "master":
-                likelyGender = "M";
+                gender = "M";
                 break;
             case "miss":
-                likelyGender = "F";
-                break;
             case "mrs":
-                likelyGender = "F";
-                break;
             case "ms":
-                likelyGender = "F";
-                break;
             case "lady":
-                likelyGender = "F";
-                break;
             case "madam":
-                likelyGender = "F";
+                gender = "F";
                 break;
-
         }
-        return likelyGender;
+        return gender;
     }
 
+    @Test
+    public void SwitchOnShortCode(){
+        assertEquals("United Kingdom", ShortCode("Uk"));
+        assertEquals("United States", ShortCode("uS"));
+        assertEquals("United States", ShortCode("uSa"));
+        assertEquals("France", ShortCode("FR"));
+        assertEquals("Sweden", ShortCode("se"));
+        assertEquals("Rest Of World", ShortCode("SA"));
+        assertEquals("Rest Of World", ShortCode("eS"));
+    }
+
+    private String ShortCode(String code) {
+
+        String country;
+        switch (code.toLowerCase()){
+            case "uk":
+                country = "United Kingdom";
+                break;
+            case "us":
+            case "usa":
+                country = "United States";
+                break;
+            case "fr":
+                country = "France";
+                break;
+            case "se":
+                country = "Sweden";
+                break;
+            default:
+                country = "Rest Of World";
+                break;
+        }
+        return country;
+    }
+
+    @Test
+    public void SwitchOnInt(){
+        assertEquals("One", NumberCode(1));
+        assertEquals("Two", NumberCode(2));
+        assertEquals("Three", NumberCode(3));
+        assertEquals("Four", NumberCode(4));
+        assertEquals("Too small", NumberCode(-5));
+        assertEquals("Too big", NumberCode(7));
+    }
+
+    private String NumberCode(int i) {
+
+        switch (i) {
+            case (1):
+                return "One";
+            case (2):
+                return "Two";
+            case (3):
+                return "Three";
+            case (4):
+                return "Four";
+            default:
+                if (i < 1) {
+                    return "Too small";
+                }
+                if (i > 4) {
+                    return "Too big";
+                }
+        }
+        return "";
+    }
 }
