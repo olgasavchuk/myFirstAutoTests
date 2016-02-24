2c6a95ccabb9da0f45180870cfb7a5a88068f617
olgasavchuk <Qaolga1!>
2016-02-24 14:59:00 +0300

    operations with array (copy)

--- src/test/java/com/chap009/DomainUsersTest.java
+++ src/test/java/com/chap009/DomainUsersTest.java
@@ -3,7 +3,7 @@ package com.chap009;
 
 import com.domainentities.User;
 import org.junit.Test;
-
+import java.util.Arrays;
 import static org.junit.Assert.*;
 
 public class DomainUsersTest {
@@ -49,4 +49,46 @@ public class DomainUsersTest {
         assertEquals(peopleIndex,11);
         System.out.print("Life is great!");
     }
+
+    @Test
+    public void MakeCopyOfArray(){
+        String[] workdays = {"Monday","Tuesday","Wednesday","Thursday","Friday"};
+        String[] weekDays;
+
+        weekDays = Arrays.copyOf(workdays, 7);
+        assertEquals(null, weekDays[5]);
+        assertEquals(null, weekDays[6]);
+        weekDays[5] = "Saturday";
+        weekDays[6] = "Sunday";
+        assertEquals("Saturday", weekDays[5]);
+        assertEquals("Sunday", weekDays[6]);
+
+        String[] threeDays;
+        threeDays = Arrays.copyOf(workdays, 3);
+        assertEquals("Monday",	threeDays[0]);
+        assertEquals("Tuesday",	threeDays[1]);
+        assertEquals("Wednesday", threeDays[2]);
+
+        String[] someDays;
+        someDays = Arrays.copyOfRange(workdays, 1, 4);
+        //Assert.copyOfRange(arrayToCopy,startIndex,endItemCount);
+        // The	 startIndex	is the	first	item	in	the	array	that	you	want	to	copy.
+        // The	 endItemCount	is	the	index	+	1	that	you	want	to	copy.
+        //e.g.	if	I	want	to	copy items 3 to 5 inclusive	(“Wednesday”,	“Thursday”,	“Friday”),
+        // then	I would	start	the	copy	from	2	(the	index	of	the	third	item),
+        // and	end	the	copy on	5	(even though	the	index	of	the	fifth	item	is	4).
+        assertEquals(3, someDays.length);
+        assertEquals("Tuesday", someDays[0]);
+        assertEquals("Wednesday", someDays[1]);
+        assertEquals("Thursday", someDays[2]);
+
+        String[] someMoreDays;
+        someMoreDays = Arrays.copyOfRange(workdays, 1, 8);
+        assertEquals(7, someMoreDays.length);
+        assertEquals(null, someMoreDays[4]);
+        assertEquals(null, someMoreDays[5]);
+        assertEquals(null, someMoreDays[6]);
+
+    }
+
 }
